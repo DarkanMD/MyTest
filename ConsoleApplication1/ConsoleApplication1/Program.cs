a2c9b297fd4f4d7fd6fd6079973c1342f9005de9
vladimir.pitei <vladimir.pitei@MDDSK40034-2>
2017-02-28 17:23:20 +0200

    cw add in master

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            string x = "Hello World";
+            Console.WriteLine(x);
         }
     }
 }
